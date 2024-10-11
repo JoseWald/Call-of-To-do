69f0f054cf964a454fceb63ae4ae823c3675023d
unknown <rajaonarivonyjosewald@gmail.com>
2024-10-11 17:42:25 +0300

    completing the AxeAttack srcipt to introduce the mecanism of melee

---
+++ Player/AxeAttack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeAttack : MonoBehaviour
+{
+    public float damage;
+
+    public float radius=1f;
+
+    public LayerMask layerMask;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] hits=Physics.OverlapSphere(transform.position , radius , layerMask);
+
+        if(hits.Length > 0){
+            Debug.Log("We touched the"+ hits[0].gameObject.tag);
+
+            gameObject.SetActive(false);
+        }
+    }
+}
